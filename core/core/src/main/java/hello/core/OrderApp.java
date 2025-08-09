8b08125197fb43b56813d753a794c634d97f2d0c
Juwoojae <juwqq1234@naver.com>
2025-08-09 19:25:52 +0900

    스프링 컨테이너

--- core/core/src/main/java/hello/core/OrderApp.java
+++ core/core/src/main/java/hello/core/OrderApp.java
@@ -7,12 +7,16 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 //주문 - 할인 도메인이 잘 구현이 되었는지 테스트하는 순수 자바 코드
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig =new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId =1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
