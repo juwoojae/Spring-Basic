748ef440b202477aea1ef88fbb9ef0b47e382994
Juwoojae <juwqq1234@naver.com>
2025-08-12 20:28:44 +0900

    싱글톤 test

--- core/core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ core/core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -20,51 +20,57 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationContextExtendsFindTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
     @Test
     @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 중복 오류가 발생한다")
-    void findBeanByParentTypeDuplicate(){
+    void findBeanByParentTypeDuplicate() {
         assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(DiscountPolicy.class));
     }
+
     @Test  //중복오류를 피하는 2가지 방법
     //1. Bean이름을 호출한다
     @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 중복 오류가 발생한다")
-    void findBeanByParentTypeName(){
+    void findBeanByParentTypeName() {
         DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
         assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
     }
+
     @Test
     @DisplayName("특정 하위 타입으로 조회")
-    void findBeanBySubTypeBeanName(){  //구체적이 하위 타입을 호출한다
-        RateDiscountPolicy bean =ac.getBean(RateDiscountPolicy.class);
+    void findBeanBySubTypeBeanName() {  //구체적이 하위 타입을 호출한다
+        RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
         assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
     }
+
     @Test
     @DisplayName("특정 하위 타입으로 조회")
-    void findAllBeanByParentType(){  //구체적이 하위 타입을 호출한다
+    void findAllBeanByParentType() {  //구체적이 하위 타입을 호출한다
         Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
         assertThat(beansOfType.size()).isEqualTo(2);
         for (String key : beansOfType.keySet()) {
             System.out.println("key = " + key + " value = " + beansOfType.get(key));
         }
     }
+
     @Test
     @DisplayName("부모 타입으로 모두 조회하기 - Object")
-    void findAllBeanObjectType(){
+    void findAllBeanObjectType() {
         Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
-       // assertThat(beansOfType.size()).isEqualTo(2);
+        // assertThat(beansOfType.size()).isEqualTo(2);
         for (String key : beansOfType.keySet()) {
             System.out.println("key = " + key + " value = " + beansOfType.get(key));
         }
     }
 
     @Configuration
-    static class TestConfig{
+    static class TestConfig {
         @Bean
-        public DiscountPolicy rateDiscountPolicy(){
+        public DiscountPolicy rateDiscountPolicy() {
             return new RateDiscountPolicy();    //자식타입의 (구현체) 가 호출이 된다
         }
+
         @Bean
-        public DiscountPolicy fixDiscountPolicy(){
+        public DiscountPolicy fixDiscountPolicy() {
             return new FixDiscountPolicy();     //자식타입의 (구현체) 가 호출이 된다
         }
     }
--- core/core/src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ core/core/src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -22,20 +22,20 @@ public class ApplicationContextSameBeanFindTest {
 
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
-    void findBeanTypeDuplicate(){
+    void findBeanTypeDuplicate() {
         assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(MemberRepository.class));
     }
 
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다")
-    void findByName(){
+    void findByName() {
         MemberRepository memberRepository = ac.getBean("memberRepository1", MemberRepository.class);
         assertThat(memberRepository).isInstanceOf(MemberRepository.class);
     }
 
     @Test
     @DisplayName("특정 타입을 모두 조회하기")
-    void findAllBeanByType(){
+    void findAllBeanByType() {
         Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
         for (String key : beansOfType.keySet()) {
             System.out.println("key = " + key + "value = " + beansOfType.get(key));
@@ -43,15 +43,17 @@ public class ApplicationContextSameBeanFindTest {
         System.out.println("beanOfType = " + beansOfType);
         assertThat(beansOfType.size()).isEqualTo(2);
     }
+
     @Configuration
-    static class SameBeanConfig{
+    static class SameBeanConfig {
 
         @Bean
-        public MemberRepository memberRepository1(){
+        public MemberRepository memberRepository1() {
             return new MemoryMemberRepository();
         }
+
         @Bean
-        public MemberRepository memberRepository2(){
+        public MemberRepository memberRepository2() {
             return new MemoryMemberRepository();
         }
     }
